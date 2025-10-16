f5f2c9c3ea2f0c08242ca83a8b96b5554851fb83
AnisHerdev <88375763+AnisHerdev@users.noreply.github.com>
2025-10-16 20:09:05 +0530

    added insta link and removed other socials

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,9 @@ const Footer: React.FC = () => {
         <footer className="bg-stone-200 dark:bg-black/20 mt-12">
             <div className="container mx-auto px-4 py-6 text-center text-stone-600 dark:text-gray-400">
                 <div className="flex justify-center space-x-6 mb-4">
-                    <a href="#" className="hover:text-amber-700 dark:hover:text-yellow-500 transition-colors"><TwitterIcon /></a>
-                    <a href="#" className="hover:text-amber-700 dark:hover:text-yellow-500 transition-colors"><InstagramIcon /></a>
-                    <a href="#" className="hover:text-amber-700 dark:hover:text-yellow-500 transition-colors"><FacebookIcon /></a>
+                    {/* <a href="#" className="hover:text-amber-700 dark:hover:text-yellow-500 transition-colors"><TwitterIcon /></a> */}
+                    <a href="https://www.instagram.com/poethra.rvu/" className="hover:text-amber-700 dark:hover:text-yellow-500 transition-colors"><InstagramIcon /></a>
+                    {/* <a href="#" className="hover:text-amber-700 dark:hover:text-yellow-500 transition-colors"><FacebookIcon /></a> */}
                 </div>
                 <p>&copy; {new Date().getFullYear()} Poéthra Creative Writing Club. All Rights Reserved.</p>
                 <p className="text-sm mt-1">Contact us at: <a href="mailto:contact@poethra.com" className="underline hover:text-amber-700 dark:hover:text-yellow-500">contact@poethra.com</a></p>
